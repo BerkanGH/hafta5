41dc9e58b01f1faf061114958316327176bcbb98
BerkanGH <berkankeskin28@gmail.com>
2024-11-24 21:46:31 +0300

    odev

---
+++ hafta5trycath/trycath/trycath/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Lütfen bir sayı giriniz:");
+
+        try
+        {
+            // Kullanıcıdan input alıyoruz
+            string input = Console.ReadLine();
+
+            // int e dönüştürdük
+            int number = int.Parse(input);
+
+            // Sayının karesini hesaplayıp veriyoruz
+            Console.WriteLine($"Girdiğiniz sayının karesi: {number * number}");
+        }
+        catch (FormatException)
+        {
+            // Geçersiz giriş durumunda hata mesajı
+            Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+        }
+    }
+}
